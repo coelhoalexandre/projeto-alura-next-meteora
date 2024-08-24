8c71207030dffc7ec0334efb90c92509b378897d
Alexandre Coelho <alexandre.uso.pessoal@gmail.com>
2024-08-24 17:22:04 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    loader: "custom",
+    loaderFile: "./src/app/imagem.js",
+    //   remotePatterns: [
+    //     {
+    //       protocol: "https",
+    //       hostname: "raw.githubusercontent.com",
+    //       pathname: "**",
+    //     },
+    //   ],
+  },
+};
+
+export default nextConfig;
---
+++ src/app/produto/[slug]/page.jsx
@@ -0,0 +1,36 @@
+import styles from "./page.module.css";
+import Produto from "@/app/components/Produto";
+
+async function getProduto(slug) {
+  const res = await fetch(
+    "https://api.npoint.io/785efd8706abb840aff4/produtos"
+  );
+
+  const produtos = await res.json();
+
+  const produto = produtos.find((produto) => produto.id.toString() === slug);
+
+  return produto;
+}
+
+export default async function ProdutoPage({ params }) {
+  const produto = await getProduto(params.slug);
+
+  return (
+    <main className={styles.main}>
+      <Produto produto={produto} />
+    </main>
+  );
+}
+
+export async function generateStaticParams(params) {
+  const res = await fetch(
+    "https://api.npoint.io/785efd8706abb840aff4/produtos"
+  );
+
+  const produtos = await res.json();
+
+  const result = produtos.map((produto) => ({ slug: produto.id.toString() }));
+
+  return result;
+}
